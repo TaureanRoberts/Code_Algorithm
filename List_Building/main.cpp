935b3a83bdefbbd55c5bc70d0717429298bb4d90
Taurean Roberts <taureanjroberts@gmail.com>
2018-01-24 09:52:08 -0600

    Asserting tests
    Make sure that it actually works.

--- List_Building/main.cpp
+++ List_Building/main.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 #include  "ListType.h"
+#include "assert.h"
 
-//int main()
-//{
-//
-//	//LinkedListType<int> *TestType = new LinkedListType<int>();
-//	//LinkedListType<int> *Testing = new LinkedListType<int>();
-//	//TestType->InitializeList();
-//
-//	//TestType->InsertFirst(1);
-//	//TestType->InsertFirst(2);
-//	//TestType->InsertFirst(3);
-//	//TestType->InsertFirst(4);
-//
-//	//TestType->InsertLast(5);
-//	//TestType->InsertLast(6);
-//	//TestType->InsertLast(7);
-//	//TestType->InsertLast(8);
-//	//
-//	//Testing->InsertFirst(1);
-//	//Testing->InsertLast(22);
-//	//TestType = Testing;
-//
-//	//TestType->Print();
-//	////Testing->Print();
-//
-//	//LinkedListType<int>test(*TestType);
-//	//
-//	//system("pause");
-//}
+int main()
+{
+
+	LinkedListType<int> *TestType = new LinkedListType<int>();
+	LinkedListType<int> *Testing = new LinkedListType<int>();
+	TestType->InitializeList();
+
+	TestType->InsertFirst(1);
+	TestType->InsertFirst(2);
+	TestType->InsertFirst(3);
+	TestType->InsertFirst(4);
+
+	TestType->InsertLast(5);
+	TestType->InsertLast(6);
+	TestType->InsertLast(7);
+	TestType->InsertLast(8);
+	
+	Testing->InsertFirst(1);
+	Testing->InsertLast(22);
+	TestType = Testing;
+
+	TestType->Print();
+
+	LinkedListType<int>test(*TestType);
+	
+	LinkedListIterator<int> *someA = new LinkedListIterator<int>();
+	LinkedListIterator<int> *someB = new LinkedListIterator<int>();
+
+	int i = 2;
+
+	//assert((someA  ) == true);
+
+	system("pause");
+}
