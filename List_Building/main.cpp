46bc19e340a15495b2b53ddeb6b1db0d0685dbd0
Taurean Roberts <taureanjroberts@gmail.com>
2018-01-31 11:50:50 -0600

    testing functions

--- List_Building/main.cpp
+++ List_Building/main.cpp
@@ -10,9 +10,11 @@ int main()
 	TestType->InitializeList();
 
 	TestType->InsertFirst(1);
+	TestType->InsertFirst(45);
 
 	TestType->InsertLast(5);
-	
+	TestType->InsertLast(14);
+
 	Testing->InsertFirst(1);
 	Testing->InsertLast(22);
 	TestType = Testing;
@@ -24,9 +26,7 @@ int main()
 	LinkedListIterator<int> *someA = new LinkedListIterator<int>();
 	LinkedListIterator<int> *someB = new LinkedListIterator<int>();
 
-	int i = 2;
+	assert(TestType->Search(45) == true);
+	assert(TestType->Search(14) == true);
 
-	//assert((someA  ) == true);
-
-	system("pause");
 }
